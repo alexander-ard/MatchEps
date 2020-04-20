bd560fc1b4b4c6efe5aeacddced94886f0696ffc
ORLAND07 <orlandorivasrubio@live.com>
2020-04-19 21:48:04 -0500

    architecture for backend

---
+++ BackEnd/matcheps/src/test/java/com/poli/matcheps/MatchepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poli.matcheps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
