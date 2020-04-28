69df2f5516f7b89d5738b3ca9ea4972f239e2c6c
alexander-ard <alexanderardmirola@gmail.com>
2020-04-28 01:59:34 -0500

    Added navbar component

---
+++ FrontEnd/matcheps/src/components/core/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="nav" v-if="!inLanding">
+    <div class="nav__btnGroup__btn nav__logo">
+      MatchEPS
+    </div>
+    <div class="nav__btnGroup">
+      <button class="nav__btnGroup__btn">
+          Lorem
+      </button>
+      <button class="nav__btnGroup__btn">
+          Lorem
+      </button>
+      <button class="nav__btnGroup__btn">
+          Lorem
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  methods: {
+
+  },
+  computed: {
+    inLanding() {
+      return this.$route.name === 'landing'
+    }
+  }
+}
+</script>
