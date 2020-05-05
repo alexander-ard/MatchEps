c36a1f471b67ece95943b7ae08f94c610cda3642
alexander-ard <alexanderardmirola@gmail.com>
2020-05-04 19:45:17 -0500

    fixed dependency order

--- FrontEnd/matcheps/src/index.js
+++ FrontEnd/matcheps/src/index.js
@@ -1,9 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import '@fortawesome/fontawesome-free/js/all';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-import '@fortawesome/fontawesome-free/js/all';
 
 Vue.config.productionTip = process.env.NODE_ENV === 'production';
 
