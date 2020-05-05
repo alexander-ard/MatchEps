eb153a4dd07329767c5672f91a0c25e3d0cae283
alexander-ard <alexanderardmirola@gmail.com>
2020-05-05 16:05:41 -0500

    Added default options for LineChart

--- FrontEnd/matcheps/src/components/charts/LineChart.vue
+++ FrontEnd/matcheps/src/components/charts/LineChart.vue
@@ -3,9 +3,25 @@ import { Line } from 'vue-chartjs';
 
 export default {
   extends: Line,
-  props: ['data', 'options'],
+  props: ['data', 'options', 'title'],
   mounted() {
-    this.renderChart(this.data, this.options);
+    const defaultOptions = {
+      responsive: true,
+      maintainAspectRatio: false,
+      fill: false,
+      steppedLine: true,
+      title: {
+        display: true,
+        text: this.title,
+        fontSize: 18
+      },
+    };
+
+    if (this.options) {
+      this.defaultOptions = Object.assign(this.options, defaultOptions);
+    }
+
+    this.renderChart(this.data, defaultOptions);
   },
 };
 </script>
